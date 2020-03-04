67ea356a77eafd6b20f9c29bb25064d95ecb4856
Misha <mishabeheka@gmail.com>
2020-03-04 16:21:38 +0200

    first commit

---
+++ src/main/java/boot/project/analyze_reviews_from_amazon/AnalyzeReviewsFromAmazonApplication.java
@@ -0,0 +1,13 @@
+package boot.project.analyze_reviews_from_amazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzeReviewsFromAmazonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyzeReviewsFromAmazonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boot/project/analyze_reviews_from_amazon/AnalyzeReviewsFromAmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.project.analyze_reviews_from_amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzeReviewsFromAmazonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
