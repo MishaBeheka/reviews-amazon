008e57164d3d37b3301aee165c2faf991418b8bc
Misha <mishabeheka@gmail.com>
2020-03-10 12:57:29 +0200

    Deleted param "Model" from method showUsers

--- src/main/java/boot/project/analyze_reviews_from_amazon/controller/ActivityUsersController.java
+++ src/main/java/boot/project/analyze_reviews_from_amazon/controller/ActivityUsersController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import boot.project.analyze_reviews_from_amazon.service.serviceImpl.ReviewServiceImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,7 @@ public class ActivityUsersController {
     }
 
     @GetMapping
-    public List<String> showUsers(Model model) {
+    public List<String> showUsers() {
         Pageable pageable = PageRequest.of(0, 10);
         return reviewService.showActivityUsers(pageable);
     }
