9c19b78db9bf6c15c845e3e81eed843d1f2e5e57
Misha <mishabeheka@gmail.com>
2020-03-10 11:35:39 +0200

    Find activity users

--- src/main/java/boot/project/analyze_reviews_from_amazon/repository/ReviewRepository.java
+++ src/main/java/boot/project/analyze_reviews_from_amazon/repository/ReviewRepository.java
@@ -1,8 +1,15 @@
 package boot.project.analyze_reviews_from_amazon.repository;
 
+import java.util.List;
+
 import boot.project.analyze_reviews_from_amazon.entity.Review;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    @Query(value = "SELECT r.profileName, COUNT(r.profileName) AS activ FROM Review r "
+            + "GROUP BY r.profileName ORDER BY activ DESC")
+    List<String> showActivityUsers(Pageable pageable);
 }
--- src/main/java/boot/project/analyze_reviews_from_amazon/service/ReviewService.java
+++ src/main/java/boot/project/analyze_reviews_from_amazon/service/ReviewService.java
@@ -1,7 +1,12 @@
 package boot.project.analyze_reviews_from_amazon.service;
 
+import java.util.List;
+
 import boot.project.analyze_reviews_from_amazon.entity.Review;
+import org.springframework.data.domain.Pageable;
 
 public interface ReviewService {
     void saveAll(Iterable<Review> iterable);
+
+    List<String> showActivityUsers(Pageable pageable);
 }
